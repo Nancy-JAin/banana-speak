b8a30cbdfc8b8bd49e7a8bc91af0890cc172cc9a
Nancy JAin <nancynj15@gmail.com>
2021-01-25 00:16:32 +0530

    update: api called and error handler

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ var btnTranslate= document.querySelector("#trans-btn");
 var txtIp= document.querySelector("#txt-ip");
 var txtOp= document.querySelector("#txt-op");
 
-function clickHandler () {
-    txtOp.innerText = "translated " +txtIp.value; 
+var serverUrl= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function constructUrl(text) {
+    return serverUrl + "?" + "text=" + text;
 }
 
+function errorHandler (error){
+    console.log("error occured", error);
+    alert("come after some time");
+}
+
+function clickHandler () {
+
+    fetch(constructUrl(txtIp.value))
+    .then(response => response.json())
+    .then(json => {
+        var translatedTxt = json.contents.translated;
+        txtOp.innerText = translatedTxt;
+    })
+    .catch(errorHandler)
+}
 
 
 btnTranslate.addEventListener("click", clickHandler)
